575924305e36307e0c60c5d5eb6dcd15280d08cf
fabaindaiz <fabiandiaz@ug.uchile.cl>
2024-12-29 02:17:11 -0300

    exit builtin

--- app/main.py
+++ app/main.py
@@ -7,7 +7,14 @@ def main():
         sys.stdout.write("$ ")
 
         # Wait for user input
-        user_input = input()
+        user_input = input().split(" ")
+
+        command = user_input[0]
+
+        match user_input:
+            case "exit":
+                exit_code = int(command[1])
+                sys.exit(exit_code)
 
         sys.stdout.write(f"{user_input}: command not found\n")
 
