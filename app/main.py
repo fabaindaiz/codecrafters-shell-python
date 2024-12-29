7df54372501912dcec9165cf4655f654c0a99ab7
fabaindaiz <fabiandiaz@ug.uchile.cl>
2024-12-29 02:48:17 -0300

    run a program

--- app/main.py
+++ app/main.py
@@ -22,29 +22,27 @@ def main():
         command = parsed_input[0]
         params = parsed_input[1:]
 
-        if command in BUILTIN:
-            match command:
-                case "type":
-                    # hardcoded for now
-                    path_location = search_file_in_path(params[0])
-                    if params[0] in BUILTIN:
-                        sys.stdout.write(f"{params[0]} is a shell builtin\n")
-                    elif path_location:
-                        sys.stdout.write(f"{params[0]} is {path_location}\n")
-                    else:
-                        sys.stdout.write(f"{params[0]}: not found\n")
-                case "exit":
-                    exit_code = int(params[0])
-                    sys.exit(exit_code)
-                case "echo":
-                    sys.stdout.write(" ".join(params) + "\n")
-            return
-        
-        path_location = search_file_in_path(command)
-        if path_location:
-            os.system(f"{path_location} {' '.join(params)}")
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
+        match command:
+            case "type":
+                # hardcoded for now
+                path_location = search_file_in_path(params[0])
+                if params[0] in BUILTIN:
+                    sys.stdout.write(f"{params[0]} is a shell builtin\n")
+                elif path_location:
+                    sys.stdout.write(f"{params[0]} is {path_location}\n")
+                else:
+                    sys.stdout.write(f"{params[0]}: not found\n")
+            case "exit":
+                exit_code = int(params[0])
+                sys.exit(exit_code)
+            case "echo":
+                sys.stdout.write(" ".join(params) + "\n")
+            case _:
+                path_location = search_file_in_path(command)
+                if path_location:
+                    os.system(f"{path_location} {' '.join(params)}")
+                else:
+                    sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
