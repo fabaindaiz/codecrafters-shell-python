20807862a927d0661d4069a49accd95a241140ad
fabaindaiz <fabiandiaz@ug.uchile.cl>
2024-12-29 14:33:51 -0300

    redirect stdout

--- app/main.py
+++ app/main.py
@@ -152,11 +152,11 @@ def main():
         
         command_file = search_file_in_path(command)
         if command_file:
-            os.system(user_input)
-            #popen_args = args
-            #popen = subprocess.Popen(args=popen_args, executable=command_file, stdout=subprocess.PIPE)
-            #redirect(popen.stdout.read().decode())
-            #popen.wait()
+            #os.system(user_input)
+            popen_args = filter_redirect(user_input)
+            popen = subprocess.Popen(args=popen_args, stdout=subprocess.PIPE, shell=True)
+            redirect(popen.stdout.read().decode())
+            popen.wait()
             continue
         
         sys.stdout.write(f"{command}: command not found\n")
